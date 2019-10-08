82e0252b7b1a3735b5b4cf17e224159b58cf38b0
Tom Spee <speetje33@gmail.com>
2019-10-08 08:40:08 +0200

    Add files via upload
    Initial commit

---
+++ Segmented13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segmented13
+//
+//  Created by Tom Spee on 07/10/2019.
+//  Copyright © 2019 Tom Spee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
